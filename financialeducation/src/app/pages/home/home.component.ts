07c2251b4321d3609f780fb7ae1a4d63655f2f62
YudexiOrozco <86724658+YudexiOrozco@users.noreply.github.com>
2022-01-21 11:11:50 -0600

    Main modal modifications

--- financialeducation/src/app/pages/home/home.component.ts
+++ financialeducation/src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-// import { LoginComponent } from '../../auth/login/login.component';
 
 @Component({
   selector: 'app-home',
@@ -10,17 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
-  // openModal () { 
-  //   const modalRef = this.modalService.open (LoginComponent, 
-  //     { 
-  //       scrollable: true,
-  //       windowClass: 'myCustomModalClass', 
-  //       keyboard: false,
-  //       backdrop: 'static'
-  //     });
-      
-  //   }
+  
   ngOnInit(): void {
   }
 
